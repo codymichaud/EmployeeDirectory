071d0e2700380fdc4a14c32d688ed2109ef4af42
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-01 09:43:00 -0800

    finished manager questions. working on engineer questions

--- index.js
+++ index.js
@@ -7,5 +7,37 @@ const managerQues = [
         type: 'input',
         name: 'name',
         message: 'What is your managers name?'
+    },
+    {
+        type: 'input',
+        name: 'employID',
+        message: 'Please enter your employee id number.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is your office number?'
+    }
+];
+
+const engineerQuest = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the first engineer.'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the id number of the employee?'
+    },
+    {
+        type: 'input',
+
     }
 ]
